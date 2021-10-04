94516ad45851225af1bd99f3e3326b00c00be379
Nati <nnati070@gmail.com>
2021-10-04 18:43:57 +0300

    change to hashRouter

--- client/src/index.js
+++ client/src/index.js
@@ -4,21 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter , HashRouter } from "react-router-dom";
 
 import {ChatContextProvider} from './Contexts/ChatProvider'
 import {SocketContextProvider} from './Contexts/SocketProvider'
 
 
 
+
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
   <SocketContextProvider>
   <ChatContextProvider>
     <App />
     </ChatContextProvider>
     </SocketContextProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
 
